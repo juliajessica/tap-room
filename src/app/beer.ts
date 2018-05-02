0a489147918d71e0cd13b469f4d52f8d19bef009
jessica schmitz and julia sheremet <julia@>
2018-05-02 10:56:53 -0700

    install bootstrap, popper, and coadd all properties to instance in Beer object

--- src/app/beer.ts
+++ src/app/beer.ts
@@ -1,6 +1,6 @@
 export class Beer {
   public emptyKeg: boolean = false;
-  // tapNumber: number;
-  constructor(public brandName: string)
+
+  constructor(public tapNumber: number, public brandName: string, public name: string, public style: string, public percentage: string, public price: number)
   {}
 }
--- src/app/beers/beers.component.ts
+++ src/app/beers/beers.component.ts
@@ -8,16 +8,16 @@ import { Beer } from '../beer';
 })
 export class BeersComponent implements OnInit {
   beers: Beer[] = [
-    new Beer('Block 15'),
-    new Beer('Barley Browns'),
-    new Beer('Breakside'),
-    new Beer('Modern Times'),
-    new Beer('Wayfinder'),
-    new Beer('Baerlic'),
-    new Beer('Silvermoon'),
-    new Beer('Pfriem'),
-    new Beer('Ecliptic'),
-    new Beer('Angry Orchard')
+    new Beer(1,'Block 15', 'Tropical Slam Sticky Hands', 'Hazy IPA', 'ABV: 8.1%', 5),
+    new Beer(2,'Barley Browns', 'Pallet Jack', 'IPA', 'ABV: 7%', 5),
+    new Beer(3,'Breakside', 'Lunch Break', 'Session IPA', 'ABV: 4.5%', 5),
+    new Beer(4,'Modern Times', 'Fruitlands', 'Sour Tropical IPA', 'ABV: 6.5%', 6),
+    new Beer(5, 'Wayfinder', 'Hell', 'Helles Lager', 'ABV: 5%', 5),
+    new Beer(6,'Baerlic', 'Dad Beer', 'Pre-Prohibition Lager', 'ABV: 5.1%', 5),
+    new Beer(7,'Silvermoon', 'IPA 97', 'IPA', 'ABV: 6.7%', 6),
+    new Beer(8, 'pFriem', 'Black Saison', 'Black Saison', 'ABV: 6.1%', 7),
+    new Beer(9,'Ecliptic', 'Capella', 'Porter', 'ABV: 5.7%', 7),
+    new Beer(10,'Angry Orchard', 'Crisp Apple', 'Apple Cider', 'ABV: 5.8%', 6)
   ];
 
   onSelect(beer: Beer): void {
